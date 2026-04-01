49d5bd77198c3fdb7d24cccaec3b593f8bf5e63f
Vitaliy Tishik <vitisik54@gmail.com>
2026-04-01 22:29:57 +0300

    ref

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -3,7 +3,6 @@ package mate.academy
 private const val THRESHOLD = 25 //const value
 
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-    зкшм
     return numbers
         .map { if (it % 2 == 0) it / 2 else it * 2 }
         .filter { it > THRESHOLD }
